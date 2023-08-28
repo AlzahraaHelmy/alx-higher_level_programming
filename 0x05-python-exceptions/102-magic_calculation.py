4c10bff47d86e73fdc6eb1ae29bf052e6bd90227
AlzahraaHelmy <zahra.helmy19113010@gmail.com>
2023-08-28 07:26:57 -0700

    za

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    per = 0
+    for index in range(1, 3):
+        try:
+            if index > a:
+                raise Exception('Too far')
+            else:
+                per += (a ** b) / index
+        except:
+            per = b + a
+            break
+    return per
