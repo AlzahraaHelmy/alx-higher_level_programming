ab3a8bfca1e47de04598f77871dfa43a67e232b4
AlzahraaHelmy <zahra.helmy19113010@gmail.com.com>
2023-09-12 11:11:56 -0700

    koka

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" create student class """
+
+
+class Student:
+    """ Defining a class of student"""
+    def __init__(self, first_name, last_name, age):
+        """ Initializing a class of student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ return a dictionary representation of class """
+        if (type(attrs)) == list and all(type(i) == str for i in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """update all the attributtes of student class"""
+        for i in json:
+            self.__dict__.update({i: json[i]})
