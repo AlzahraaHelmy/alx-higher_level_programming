18f72a59bbc191fcebad338af628bf7c1d937cd8
AlzahraaHelmy <zahra.helmy19113010@gmail.com.com>
2023-09-12 09:38:50 -0700

    koka

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module defines a function from string to JSON"""
+import json
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of a (string) object"""
+    return json.dumps(my_obj)
